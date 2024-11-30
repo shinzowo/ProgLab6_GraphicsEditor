476bb2abf03e04bdece7ff948295a2a73114e189
Shinzo <shinzouwoshi@gmail.com>
2024-11-30 15:10:40 +0700

    completed prototype with basic UI

--- main.cpp
+++ main.cpp
@@ -2,11 +2,39 @@
 #include "GraphicsController.h"
 #include "canvas.h"
 #include "canvasView.h"
+void show_main_menu();
+void create_canvas(GraphicsController& controller);
+void import_canvas(GraphicsController& controller);
+void export_canvas(GraphicsController& controller);
+void add_shape(GraphicsController& controller);
+void delete_shape(GraphicsController& controller);
+void render_view(GraphicsController& controller);
 int main(){
+    std::cout<<"Welcome to the Vector Graphics Editor\n";
+    
     auto canvas=std::make_shared<Canvas>();
     auto view = std::make_shared<CanvasView>();
     GraphicsController controller(canvas, view);
 
+    int choice=-1;
+    while(choice!=0){
+        show_main_menu();
+        std::cin>>choice;
+        switch(choice){
+            case 1: create_canvas(controller); break;
+            case 2: import_canvas(controller);break;
+            case 3: export_canvas(controller);break;
+            case 4: add_shape(controller); break;
+            case 5: delete_shape(controller); break;
+            case 6: render_view(controller); break;
+            case 0: 
+            std::cout<<"Exit is completed"; 
+            break;
+            default:
+            std::cout<<"Incorrect choice. Try again.\n";
+        }
+    }
+    /*
     controller.createNewCanvas();
     controller.addShape(std::make_shared<Line>());
     controller.addShape(std::make_shared<Circle>());
@@ -15,6 +43,68 @@ int main(){
     controller.exportCanvas();
 
     controller.deleteShape(0);
-    controller.renderView();
+    controller.renderView();*/
     return 0;
+}
+void show_main_menu(){
+    std::cout<<"\nMenu:\n"
+    << "1. Create a new canvas\n"
+    << "2. Import a aanvas\n"
+    << "3. Export a canvas\n"
+    << "4. Add shape\n"
+    << "5. Delete shape\n"
+    << "6. Show canvas\n"
+    << "0. Exit\n"
+    << "Command: ";
+}
+
+void create_canvas(GraphicsController &controller)
+{
+    controller.createNewCanvas();
+}
+
+void import_canvas(GraphicsController &controller)
+{
+    std::cout <<"Enter data for import:\n";
+    std::string data;
+    std::getline(std::cin, data);
+    controller.importCanvas(data);
+}
+
+void export_canvas(GraphicsController &controller)
+{
+    controller.exportCanvas();
+}
+
+void add_shape(GraphicsController &controller)
+{
+    std::cout <<"Choose shape to add on the canvas\n"
+    <<"1.Line\n2.Circle\n3.Rectangle\n:";
+    int choice;
+    std::cin>>choice;
+    if(choice==1){
+        controller.addShape(std::make_shared<Line>());
+    }
+    else if(choice==2){
+        controller.addShape(std::make_shared<Circle>());
+    }
+    else if(choice==3){
+        controller.addShape(std::make_shared<Rectangle>());
+    }
+    else{
+        std::cout <<"invalid choice.\n";
+    }
+}
+
+void delete_shape(GraphicsController &controller)
+{
+    std::cout <<"Enter index:\n";
+    size_t index;
+    std::cin>>index;
+    controller.deleteShape(index);
+}
+
+void render_view(GraphicsController &controller)
+{
+    controller.renderView();
 }
